9ec38e2c85b9176e3725eae19191fe542e810f5a
wirachai <wirachai@iconext.co.th>
2017-07-13 15:48:33 +0700

    Add project files.

---
+++ CleanCodeWorkshop/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCodeWorkshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
